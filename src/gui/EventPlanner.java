05be62db32c0c8f6565a54713a64b26469694a0e
Kevin27182 <kevinmnard@gmail.com>
2024-09-20 20:54:09 -0500

    added skeleton for EventPlanner.java

--- src/gui/EventPlanner.java
+++ src/gui/EventPlanner.java
@@ -2,10 +2,25 @@ package gui;
 
 import base.*;
 import javax.swing.*;
+import java.awt.*;
 
 public class EventPlanner extends javax.swing.JPanel {
+    private static final int PREFERRED_WIDTH = 600;
+    private static final int PREFERRED_HEIGHT = 800;
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("EventPlanner");
+        // Create and configure JFrame object
+        JFrame frame = new JFrame("Event Planner");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(PREFERRED_WIDTH, PREFERRED_HEIGHT));
+
+//        // Add EventListPanel
+//        EventListPanel eventListPanel = new EventListPanel;
+//        frame.add(eventListPanel);
+
+        // Pack and display the JFrame
+        frame.pack();
+        frame.setVisible(true);
     }
 
 //    public static void addDefaultEvents(EventPanel events) {
